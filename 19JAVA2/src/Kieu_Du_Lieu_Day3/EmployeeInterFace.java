f86f082d26eb2baac753ea85fa5206efb5383850
ThanhDat1905 <thanhdat19051998@gmail.com>
2019-05-10 21:37:45 +0700

    Interface Java

---
+++ 19JAVA2/src/Kieu_Du_Lieu_Day3/EmployeeInterFace.java
@@ -0,0 +1,11 @@
+package Kieu_Du_Lieu_Day3;
+
+import java.util.List;
+
+public interface EmployeeInterFace {
+	public void addEmployee(List<Employee> list);
+	public void show(List<Employee> list);
+	public void Seach(List<Employee> list,String name);
+	public void Update(List<Employee> list,String name);
+	
+}
